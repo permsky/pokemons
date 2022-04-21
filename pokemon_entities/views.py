4e0015a9190e3ceec6df6807270172a99220ce26
Vitaly Borodulin <rainbowf@mail.ru>
2022-04-22 00:21:56 +0500

    Show pokemons on mainpage

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,8 +1,10 @@
 import folium
 import json
 
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpRequest
 from django.shortcuts import render
+
+from .models import Pokemon, PokemonEntity
 
 
 MOSCOW_CENTER = [55.751244, 37.618423]
@@ -27,25 +29,46 @@ def add_pokemon(folium_map, lat, lon, image_url=DEFAULT_IMAGE_URL):
 
 
 def show_all_pokemons(request):
-    with open('pokemon_entities/pokemons.json', encoding='utf-8') as database:
-        pokemons = json.load(database)['pokemons']
+    pokemons = Pokemon.objects.all()
 
     folium_map = folium.Map(location=MOSCOW_CENTER, zoom_start=12)
     for pokemon in pokemons:
-        for pokemon_entity in pokemon['entities']:
-            add_pokemon(
-                folium_map, pokemon_entity['lat'],
-                pokemon_entity['lon'],
-                pokemon['img_url']
-            )
+        pokemon_entities = PokemonEntity.objects.filter(id=pokemon.id)
+        for pokemon_entity in pokemon_entities:
+            if pokemon.image:
+                img_url = request.build_absolute_uri(pokemon.image.url)
+                add_pokemon(
+                    folium_map,
+                    pokemon_entity.latitude,
+                    pokemon_entity.longitude,
+                    img_url
+                )
+            else:
+                add_pokemon(
+                    folium_map,
+                    pokemon_entity.latitude,
+                    pokemon_entity.longitude,
+                )
 
     pokemons_on_page = []
     for pokemon in pokemons:
-        pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
-        })
+        if pokemon.image:
+            img_url = request.build_absolute_uri(pokemon.image.url)
+            pokemons_on_page.append(
+                {
+                    'pokemon_id': pokemon.id,
+                    'img_url': img_url,
+                    'title_ru': pokemon.title,
+                }
+            )
+        else:
+            pokemons_on_page.append(
+                {
+                    'pokemon_id': pokemon.id,
+                    'img_url': DEFAULT_IMAGE_URL,
+                    'title_ru': pokemon.title,
+                }
+            )
 
     return render(request, 'mainpage.html', context={
         'map': folium_map._repr_html_(),
