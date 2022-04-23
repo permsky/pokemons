6c2553461aecc282f62eacfdb93accbd20409c7f
Vitaly Borodulin <rainbowf@mail.ru>
2022-04-23 20:27:03 +0500

    Add verbose name for models fields

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,16 +2,28 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=30)
-    title_en = models.CharField(max_length=30, default='title_en')
-    title_jp = models.CharField(max_length=30, default='title_jp')
-    image = models.ImageField(blank=True, null=True)
-    description = models.TextField(default='Описание покемона')
+    title_ru = models.CharField('название покемона по-русски', max_length=30)
+    title_en = models.CharField(
+        'название покемона по-английски',
+        max_length=30,
+        default='title_en'
+    )
+    title_jp = models.CharField(
+        'название покемона по-японски',
+        max_length=30,
+        default='title_jp'
+    )
+    image = models.ImageField('изображение покемона', blank=True, null=True)
+    description = models.TextField(
+        'описание покемона',
+        default='описание покемона'
+    )
     evolution_form = models.ForeignKey(
         'self',
         blank=True,
         null=True,
-        on_delete=models.SET_NULL
+        on_delete=models.SET_NULL,
+        verbose_name='в кого эволюционирует'
     )
 
     def __str__(self):
@@ -22,13 +34,17 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField(default=1)
-    health = models.IntegerField(default=0)
-    strength = models.IntegerField(default=0)
-    defense = models.IntegerField(default=0)
-    endurance = models.IntegerField(default=0)
+    latitude = models.FloatField('широта')
+    longitude = models.FloatField('долгота')
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        verbose_name='покемон'
+    )
+    appeared_at = models.DateTimeField('появляется в')
+    disappeared_at = models.DateTimeField('исчезает в')
+    level = models.IntegerField('уровень', default=1)
+    health = models.IntegerField('здоровье', default=0)
+    strength = models.IntegerField('сила', default=0)
+    defense = models.IntegerField('защита', default=0)
+    endurance = models.IntegerField('выносливость', default=0)
